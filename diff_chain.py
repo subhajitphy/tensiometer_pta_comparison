ce704a36d0b43daf4671ff62bed825a538c9062b
Subhajit <107269864+subhajitphy@users.noreply.github.com>
2023-07-15 20:08:05 +0530

    Update diff_chain.py

--- diff_chain.py
+++ diff_chain.py
@@ -2,35 +2,35 @@ import tensiometer
 from tensiometer import mcmc_tension
 import numpy as np
 import numpy.linalg as nl
-import scipy
+import scipy, math
 from tensiometer import utilities
 mcmc_tension.n_threads = 1
 # create the distribution of the parameter differences:
 import warnings
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 
-def Diff_chain_shift(A1,A2,off_size=None,MLinfo=None,
-                     boost=None,method=None):
+def Diff_chain_shift(A1,A2,MLinfo=None,method=None):
     
     input_arr=[A1,A2]
 
-    if(off_size is None):
-        off_size=5
-    if(boost is None):
-        boost=4
-        
-    for i,chain in enumerate(input_arr):
-        chain.chain_offsets =np.linspace(0,len(input_arr[i].samples[:,0]),off_size,dtype = int)
-    diff_chain = mcmc_tension.parameter_diff_chain( A1, A2, boost=boost)
+    
+    diff_chain = mcmc_tension.parameter_diff_chain( A1, A2)
     
     if len(A1.getMeans())<=2 and method==None:
+        
         shift_probability, shift_lower, shift_upper = mcmc_tension.kde_parameter_shift_2D_fft(diff_chain, feedback=0)
-        if shift_probability==1:
-            sig_n, chi_2, D_B, chi2_probability=tension_chi2_approach(A1,A2)
-            warnings.warn('Tension is higher than 4 sigma!! chi squared estimators will be used to compute the tension.')
-            return diff_chain, sig_n, chi_2, D_B
+        nsig=utilities.from_confidence_to_sigma(shift_probability)
+        sigl=utilities.from_confidence_to_sigma(shift_lower)
+        sigh=utilities.from_confidence_to_sigma(shift_upper)
+        #nsig=utilities.from_confidence_to_sigma(shift_probability)
+        if (shift_probability!=1):
+            return diff_chain, nsig, sigl, sigh
         else:
-            return diff_chain, utilities.from_confidence_to_sigma(shift_probability), shift_lower, shift_upper
+            sig_n, chi_2, D_B, chi2_probability=tension_chi2_approach(A1,A2)
+            warnings.warn('Tension is higher!! chi squared estimators will be used to compute the tension.')
+            return diff_chain, sig_n, chi_2, D_B
+        
+
             
     if method=='ML' or len(A1.getMeans())>2:
 
@@ -47,13 +47,20 @@ def Diff_chain_shift(A1,A2,off_size=None,MLinfo=None,
 
 
         exact_shift_P_1, exact_shift_low_1, exact_shift_hi_1 = diff_flow_callback.estimate_shift()
+        nsig=utilities.from_confidence_to_sigma(exact_shift_P_1)
+        sigl=utilities.from_confidence_to_sigma(exact_shift_low_1)
+        sigh=utilities.from_confidence_to_sigma(exact_shift_hi_1)
+        
+        
+        if (exact_shift_P_1!=1):
+            return diff_chain, nsig, sigl, sigh
+        
 
-        if exact_shift_P_1==1:
+        else:
             sig_n, chi_2, D_B, chi2_probability=tension_chi2_approach(A1,A2)
             warnings.warn('Tension is higher than 4 sigma!! chi squared estimators will be used to compute the tension.')
             return diff_chain, sig_n, chi_2, D_B
-        else:
-            return diff_chain, utilities.from_confidence_to_sigma(exact_shift_P_1), exact_shift_low_1, exact_shift_hi_1
+      
         
         
         
